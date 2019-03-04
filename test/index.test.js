0ccf4995d736908057f96d28a11a3e2665a758a0
Zhu Liang <zhuliangg11@gmail.com>
2019-03-05 00:26:52 +0800

    test: add simple tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 const { macacaHelper } = window;
-const { domEvent } = window._macaca_simulate; 
+const { domEvent } = window._macaca_simulate;
 
 describe('base', () => {
   let container;
@@ -10,13 +10,56 @@ describe('base', () => {
 
   it('1', async () => {
     console.log('1');
-    await macacaHelper.sleep(1000);
+    await macacaHelper.sleep(100);
   });
 
-  it('2', async () => {
+  it('click', async () => {
     container = document.querySelector('body');
-    domEvent(container, 'click', {
+    domEvent(container, 'click', {});
+    await macacaHelper.sleep(100);
+  });
+
+  it('click when target is array', async () => {
+    container = document.querySelector('body');
+    domEvent([container], 'click', {});
+    await macacaHelper.sleep(100);
+  });
+
+  it('click when target is null', async () => {
+    container = document.querySelector('body');
+    try {
+      domEvent(null, 'click', {});
+    } catch (e) {
+      // do nothing
+    }
+    await macacaHelper.sleep(100);
+  });
+
+  it('random', async () => {
+    container = document.querySelector('body');
+    try {
+      domEvent(container, 'random', {});
+    } catch (e) {
+      // do nothing
+    }
+    await macacaHelper.sleep(100);
+  });
+
+  it('mouseout', async () => {
+    container = document.querySelector('body');
+    testElm = document.getElementById('test');
+    domEvent(container, 'mouseout', {
+      relatedTarget: testElm
     });
-    await macacaHelper.sleep(1000);
+    await macacaHelper.sleep(100);
+  });
+
+  it('mouseover', async () => {
+    container = document.querySelector('body');
+    testElm = document.getElementById('test');
+    domEvent(container, 'mouseover', {
+      relatedTarget: testElm
+    });
+    await macacaHelper.sleep(100);
   });
 });
